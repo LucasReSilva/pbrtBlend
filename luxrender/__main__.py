4e73fe76c8597721b8d08122863b0689bb52bacb
doug <doug@localhost>
2009-08-22 01:27:58 +0100

    Added some standard blender interfaces to LuxRender render settings UI

--- luxrender/__main__.py
+++ luxrender/__main__.py
@@ -5,6 +5,14 @@ from ui import Sampler_Render_Settings
 from ui import Integrator_Render_Settings
 from properties import properties
 
+# Add standard Blender Interface elements
+import buttons_scene
+buttons_scene.SCENE_PT_render.COMPAT_ENGINES.add('luxrender')
+buttons_scene.SCENE_PT_dimensions.COMPAT_ENGINES.add('luxrender')
+buttons_scene.SCENE_PT_output.COMPAT_ENGINES.add('luxrender')
+del buttons_scene
+
+# Then define all custom stuff
 class luxrender(properties, engine_base):
 	__label__ = 'LuxRender'
 		
@@ -16,3 +24,5 @@ class luxrender(properties, engine_base):
 		
 	def render(self, scene):
 		pass
+
+	
