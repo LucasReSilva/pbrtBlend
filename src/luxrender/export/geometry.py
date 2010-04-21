9a309ac82798454b51e4f4f083bbaca2347a3983
Genscher <Genscher@localhost>
2010-04-22 00:20:32 +0200

    Reorganising export_lxo()  and make exportMesh() an independant function

--- src/luxrender/export/geometry.py
+++ src/luxrender/export/geometry.py
@@ -37,7 +37,7 @@ from luxrender.export import ParamSet
 # getMeshType(self, scene, mesh, ss)
 # returns type of mesh as string to use depending on thresholds
 #-------------------------------------------------
-def getMeshType(scene, mesh):
+def getMeshType(mesh):
     
     params = ParamSet()
     dstr = 'trianglemesh'
@@ -50,6 +50,104 @@ def getMeshType(scene, mesh):
         params.add_bool('dmsharpboundary', mesh.luxrender_mesh.sharpbound)
     
     return dstr,params
+
+def exportMesh(ob, me, l, smoothing_enabled):
+    
+    # dummy material for now
+    dummy_params = ParamSet()
+    dummy_params.add_color('Kd', [
+        0.75, 0.75, 0.75
+        # just for a laugh
+        #random.random(),
+        #random.random(),
+        #random.random()
+    ])
+    l.material('matte', dummy_params)
+    
+    faces_verts = [f.verts for f in me.faces]
+    ffaces = [f for f in me.faces]
+    #faces_normals = [tuple(f.normal) for f in me.faces]
+    #verts_normals = [tuple(v.normal) for v in me.verts]
+    
+    # face indices
+    index = 0
+    indices = []
+    ntris = 0
+    for face in ffaces:
+        indices.append(index)
+        indices.append(index+1)
+        indices.append(index+2)
+        ntris += 3
+        if (len(face.verts)==4):
+            indices.append(index)
+            indices.append(index+2)
+            indices.append(index+3)
+            ntris += 3
+        index += len(face.verts)
+        
+    # vertex positions
+    points = []
+    nvertices = 0
+    for face in ffaces:
+        for vertex in face.verts:
+            v = me.verts[vertex]
+            nvertices += 1
+            for co in v.co:
+                points.append(co)
+                
+    # vertex normals
+    normals = []
+    for face in ffaces:
+        normal = face.normal
+        for vertex in face.verts:
+            if (smoothing_enabled and face.smooth):
+                normal = vertex.normal
+            for no in normal:
+                normals.append(no)
+                
+    # uv coordinates
+    try:
+        uv_layer = me.active_uv_texture.data
+    except:
+        uv_layer = None
+        
+    if uv_layer:
+        uvs = []
+        for fi, uv in enumerate(uv_layer):
+            if len(faces_verts[fi]) == 4:
+                face_uvs = uv.uv1, uv.uv2, uv.uv3, uv.uv4
+            else:
+                face_uvs = uv.uv1, uv.uv2, uv.uv3
+            for uv in face_uvs:
+                for single_uv in uv:
+                    uvs.append(single_uv)
+                    
+    
+    #print(' %s num points: %i' % (ob.name, len(points)))
+    #print(' %s num normals: %i' % (ob.name, len(normals)))
+    #print(' %s num idxs: %i' % (ob.name, len(indices)))
+    
+    # export shape        
+    shape_type, shape_params = getMeshType(ob.data)
+    
+    if l.API_TYPE == 'PURE':
+        # ntris isn't really the number of tris!!
+        shape_params.add_integer('ntris', ntris)
+        shape_params.add_integer('nvertices', nvertices)
+    
+    shape_params.add_integer('indices', indices)
+    shape_params.add_point('P', points)
+    shape_params.add_normal('N', normals)
+    
+    if uv_layer:
+        #print(' %s num uvs: %i' % (ob.name, len(uvs)))
+        shape_params.add_float('uv', uvs)
+    
+    #print(' %s ntris: %i' % (ob.name, ntris))
+    #print(' %s nvertices: %i' % (ob.name, nvertices))
+    
+    l.shape(shape_type, shape_params)
+
 
 def write_lxo(render_engine, l, scene, smoothing_enabled=True):
     '''
@@ -90,107 +188,14 @@ def write_lxo(render_engine, l, scene, smoothing_enabled=True):
         
         if not me:
             continue
-        
+
         l.attributeBegin(comment=ob.name, file=Files.GEOM)
-        
+            
         # object translation/rotation/scale 
         l.transform( matrix_to_list(ob.matrix) )
-        
-        # dummy material for now
-        dummy_params = ParamSet()
-        dummy_params.add_color('Kd', [
-            0.75, 0.75, 0.75
-            # just for a laugh
-            #random.random(),
-            #random.random(),
-            #random.random()
-        ])
-        l.material('matte', dummy_params)
-        
-        faces_verts = [f.verts for f in me.faces]
-        ffaces = [f for f in me.faces]
-        #faces_normals = [tuple(f.normal) for f in me.faces]
-        #verts_normals = [tuple(v.normal) for v in me.verts]
-        
-        # face indices
-        index = 0
-        indices = []
-        ntris = 0
-        for face in ffaces:
-            indices.append(index)
-            indices.append(index+1)
-            indices.append(index+2)
-            ntris += 3
-            if (len(face.verts)==4):
-                indices.append(index)
-                indices.append(index+2)
-                indices.append(index+3)
-                ntris += 3
-            index += len(face.verts)
-            
-        # vertex positions
-        points = []
-        nvertices = 0
-        for face in ffaces:
-            for vertex in face.verts:
-                v = me.verts[vertex]
-                nvertices += 1
-                for co in v.co:
-                    points.append(co)
-                    
-        # vertex normals
-        normals = []
-        for face in ffaces:
-            normal = face.normal
-            for vertex in face.verts:
-                if (smoothing_enabled and face.smooth):
-                    normal = vertex.normal
-                for no in normal:
-                    normals.append(no)
-                    
-        # uv coordinates
-        try:
-            uv_layer = me.active_uv_texture.data
-        except:
-            uv_layer = None
-            
-        if uv_layer:
-            uvs = []
-            for fi, uv in enumerate(uv_layer):
-                if len(faces_verts[fi]) == 4:
-                    face_uvs = uv.uv1, uv.uv2, uv.uv3, uv.uv4
-                else:
-                    face_uvs = uv.uv1, uv.uv2, uv.uv3
-                for uv in face_uvs:
-                    for single_uv in uv:
-                        uvs.append(single_uv)
-                        
-        
-        #print(' %s num points: %i' % (ob.name, len(points)))
-        #print(' %s num normals: %i' % (ob.name, len(normals)))
-        #print(' %s num idxs: %i' % (ob.name, len(indices)))
-        
-        # export shape        
-        shape_type, shape_params = getMeshType(scene, ob.data)
-        
-        if l.API_TYPE == 'PURE':
-            # ntris isn't really the number of tris!!
-            shape_params.add_integer('ntris', ntris)
-            shape_params.add_integer('nvertices', nvertices)
-        
-        shape_params.add_integer('indices', indices)
-        shape_params.add_point('P', points)
-        shape_params.add_normal('N', normals)
-        
-        if uv_layer:
-            #print(' %s num uvs: %i' % (ob.name, len(uvs)))
-            shape_params.add_float('uv', uvs)
-        
-        #print(' %s ntris: %i' % (ob.name, ntris))
-        #print(' %s nvertices: %i' % (ob.name, nvertices))
-        
-        l.shape(shape_type, shape_params)
-        
+
+        exportMesh(ob, me, l, smoothing_enabled)
+
         l.attributeEnd()
         
         bpy.data.meshes.remove(me)
@@ -200,3 +205,5 @@ def write_lxo(render_engine, l, scene, smoothing_enabled=True):
         if pc not in rpcs:
             rpcs.append(pc)
             render_engine.update_stats('', 'LuxRender: Parsing meshes %i%%' % pc)
+        
+        
