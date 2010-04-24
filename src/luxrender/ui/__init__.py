8f9b9a11fe29b99efd073e9519044a56cd935736
Doug Hammond <DougHammond@localhost>
2010-04-24 20:13:32 +0100

    Materials UI mostly finished

--- src/luxrender/ui/__init__.py
+++ src/luxrender/ui/__init__.py
@@ -75,39 +75,59 @@ class ColorTexture(object):
 		self.attr = attr
 		self.name = name
 		self.property_group = property_group
-		
+	
 	def get_controls(self):
-		return [ self.attr ]
+		return [
+			[ 0.9, ['%s_label' % self.attr, self.attr], '%s_usetexture' % self.attr ],
+			'%s_texture' % self.attr
+		]
 	
 	def get_visibility(self):
-		return { self.attr: { 'material': has_property(self.attr) } }
+		return {
+			'%s_label' % self.attr: 			{ 'material': has_property(self.attr) },
+			self.attr: 							{ 'material': has_property(self.attr) },
+			'%s_usetexture' % self.attr:		{ 'material': has_property(self.attr) },
+			'%s_texture' % self.attr:			{ 'material': has_property(self.attr), '%s_usetexture' % self.attr: True },
+		}
 	
 	def get_properties(self):
 		return [
 			{
+				'attr': '%s_usetexture' % self.attr,
+				'type': 'bool',
+				'name': 'T',
+				'description': 'Textured %s' % self.name,
+				'default': False,
+				'toggle': True,
+			},
+			{
+				'type': 'text',
+				'attr': '%s_label' % self.attr,
+				'name': self.name
+			},
+			{
 				'type': 'float_vector',
 				'attr': self.attr,
-				'name': self.name,
+				'name': '', #self.name,
 				'description': self.name,
 				'default': (0.8,0.8,0.8),
 				'subtype': 'COLOR',
-				'precision': 5,
 			},
-#			{
-#				'attr': '%s_texture' % attr,
-#				'type': 'string',
-#				'name': '%s_texture' % attr,
-#				'description': '%s_texture' % attr,
-#			},
-#			{
-#				'type': 'prop_object',
-#				'attr': attr,
-#				'src': lambda s,c: s.object.material_slots[s.object.active_material_index].material, #context_tex_datablock(s),
-#				'src_attr': 'texture_slots',
-#				'trg': lambda s,c: getattr(c, property_group),
-#				'trg_attr': '%s_texture' % attr,
-#				'name': name
-#			},
+			{
+				'attr': '%s_texturename' % self.attr,
+				'type': 'string',
+				'name': '%s_texturename' % self.attr,
+				'description': '%s Texture' % self.name,
+			},
+			{
+				'type': 'prop_object',
+				'attr': '%s_texture' % self.attr,
+				'src': lambda s,c: s.object.material_slots[s.object.active_material_index].material,
+				'src_attr': 'texture_slots',
+				'trg': lambda s,c: getattr(c, self.property_group),
+				'trg_attr': '%s_texturename' % self.attr,
+				'name': self.name
+			},
 		]
 
 class FloatTexture(object):
