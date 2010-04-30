6791d143b869524b5717889d3cc8a6c35db727b3
Genscher <Genscher@localhost>
2010-04-30 18:54:36 +0200

    Add support for group/face/vert light duplis

--- src/luxrender/export/lights.py
+++ src/luxrender/export/lights.py
@@ -67,6 +67,96 @@ def attr_light(l, name, group, type, params, transform=None):
 	else:
 		l.attributeEnd()
 
+def exportLights(l, scene, ob, matrix):
+	light = ob.data
+		
+	# Params common to all light types
+	light_params = ParamSet() \
+		.add_float('gain', light.energy) \
+		.add_float('importance', light.luxrender_lamp.importance)
+	
+	
+	if light.type == 'SUN':
+		invmatrix = mathutils.Matrix(matrix).invert()
+		light_params.add_vector('sundir', (invmatrix[0][2], invmatrix[1][2], invmatrix[2][2]))
+		light_params.add_float('turbidity', light.luxrender_lamp.turbidity)
+		# nsamples
+		# relsize (sun only)
+		# [a-e]const (sky only)
+		attr_light(l, ob.name, light.luxrender_lamp.lightgroup, light.luxrender_lamp.sunsky_type, light_params)
+
+		return True
+	
+	
+	# all lights apart from sun + sky have "color L"
+	#if lamp.luxrender_lamp.L_usetexture:
+		# TODO: find and export the named texture
+	#	light_params.add_texture('L', lamp.luxrender_lamp.L_texturename)
+	#else:
+	light_params.update( add_color_texture(l, 'L', light.luxrender_lamp, light) )
+	
+	if light.type == 'SPOT':
+		coneangle = degrees(light.spot_size) * 0.5
+		conedeltaangle = degrees(light.spot_size * 0.5 * light.spot_blend)
+		#light_params.add_color('L', list(light.color))
+		light_params.add_point('from', (0,0,0))
+		light_params.add_point('to', (0,0,-1))
+		light_params.add_float('coneangle', coneangle)
+		light_params.add_float('conedeltaangle', conedeltaangle)
+		attr_light(l, ob.name, light.luxrender_lamp.lightgroup, 'spot', light_params, transform=matrix_to_list(matrix))
+
+		return True
+
+	if light.type == 'POINT':
+		#light_params.add_color('L', list(light.color))
+		light_params.add_point('from', (0,0,0)) # (0,0,0) is correct since there is an active Transform
+		attr_light(l, ob.name, light.luxrender_lamp.lightgroup, 'point', light_params, transform=matrix_to_list(matrix))
+
+		return True
+		
+	if light.type == 'HEMI':
+		if light.luxrender_lamp.infinite_map != '':
+			light_params.add_string('mapname', light.luxrender_lamp.infinite_map)
+			light_params.add_string('mapping', light.luxrender_lamp.mapping_type)
+		#else:
+			# TODO: check if users want L multiplied to HDRI maps
+			#light_params.add_color('L', list(light.color))
+		# nsamples
+		# gamma
+		attr_light(l, ob.name, light.luxrender_lamp.lightgroup, 'infinite', light_params, transform=matrix_to_list(matrix))
+
+		return True
+	
+	if light.type == 'AREA':
+		#light_params.add_color('L', list(light.color))
+		light_params.add_float('power', light.luxrender_lamp.power)
+		light_params.add_float('efficacy', light.luxrender_lamp.efficacy)
+		# nsamples
+		l.attributeBegin(ob.name, file=Files.MAIN)
+		l.transform(matrix_to_list(matrix))
+		l.lightGroup(light.luxrender_lamp.lightgroup, [])
+		l.areaLightSource('area', light_params)
+
+		areax = light.size
+
+		if light.shape == 'SQUARE':
+			areay = areax
+		elif light.shape == 'RECTANGLE':
+			areay = light.size_y
+		else:
+			areay = areax # not supported yet
+
+		points = [-areax/2, areay/2, 0.0, areax/2, areay/2, 0.0, areax/2, -areay/2, 0.0, -areax/2, -areay/2, 0.0]
+		shape_params = ParamSet() \
+			.add_integer('indices', [0, 1, 2, 0, 2, 3]) \
+			.add_point('P', points)
+		l.shape('trianglemesh', shape_params)
+		l.attributeEnd()
+		
+		return True
+
+	return False
+
 def lights(l, scene):
 	'''
 	l				pylux.Context
@@ -90,98 +180,20 @@ def lights(l, scene):
 		if ob.parent and ob.parent.dupli_type != 'NONE':
 			continue
 
-		light = ob.data
-		
-		# Params common to all light types
-		light_params = ParamSet() \
-			.add_float('gain', light.energy) \
-			.add_float('importance', light.luxrender_lamp.importance)
-		
-		
-		if light.type == 'SUN':
-			invmatrix = mathutils.Matrix(ob.matrix).invert()
-			light_params.add_vector('sundir', (invmatrix[0][2], invmatrix[1][2], invmatrix[2][2]))
-			light_params.add_float('turbidity', light.luxrender_lamp.turbidity)
-			# nsamples
-			# relsize (sun only)
-			# [a-e]const (sky only)
-			attr_light(l, ob.name, light.luxrender_lamp.lightgroup, light.luxrender_lamp.sunsky_type, light_params)
-			have_light = True
-			
-			continue
-		
-		
-		# all lights apart from sun + sky have "color L"
-		#if lamp.luxrender_lamp.L_usetexture:
-			# TODO: find and export the named texture
-		#	light_params.add_texture('L', lamp.luxrender_lamp.L_texturename)
-		#else:
-		light_params.update( add_color_texture(l, 'L', light.luxrender_lamp, light) )
-		
-		if light.type == 'SPOT':
-			coneangle = degrees(light.spot_size) * 0.5
-			conedeltaangle = degrees(light.spot_size * 0.5 * light.spot_blend)
-			#light_params.add_color('L', list(light.color))
-			light_params.add_point('from', (0,0,0))
-			light_params.add_point('to', (0,0,-1))
-			light_params.add_float('coneangle', coneangle)
-			light_params.add_float('conedeltaangle', conedeltaangle)
-			attr_light(l, ob.name, light.luxrender_lamp.lightgroup, 'spot', light_params, transform=matrix_to_list(ob.matrix))
-			have_light = True
-			
-			continue
+		if ob.dupli_type in ('GROUP', 'VERTS', 'FACES'):
+			# create dupli objects
+			ob.create_dupli_list(scene)
 
-		if light.type == 'POINT':
-			#light_params.add_color('L', list(light.color))
-			light_params.add_point('from', (0,0,0)) # (0,0,0) is correct since there is an active Transform
-			attr_light(l, ob.name, light.luxrender_lamp.lightgroup, 'point', light_params, transform=matrix_to_list(ob.matrix))
-			have_light = True
-			
-			continue
-			
-		if light.type == 'HEMI':
-			if light.luxrender_lamp.infinite_map != '':
-				light_params.add_string('mapname', light.luxrender_lamp.infinite_map)
-				light_params.add_string('mapping', light.luxrender_lamp.mapping_type)
-			#else:
-				# TODO: check if users want L multiplied to HDRI maps
-				#light_params.add_color('L', list(light.color))
-			# nsamples
-			# gamma
-			attr_light(l, ob.name, light.luxrender_lamp.lightgroup, 'infinite', light_params, transform=matrix_to_list(ob.matrix))
-			have_light = True
-			
-			continue
-		
-		if light.type == 'AREA':
-			#light_params.add_color('L', list(light.color))
-			light_params.add_float('power', light.luxrender_lamp.power)
-			light_params.add_float('efficacy', light.luxrender_lamp.efficacy)
-			# nsamples
-			l.attributeBegin(ob.name, file=Files.MAIN)
-			l.transform(matrix_to_list(ob.matrix))
-			l.lightGroup(light.luxrender_lamp.lightgroup, [])
-			l.areaLightSource('area', light_params)
+			for dupli_ob in ob.dupli_list:
+				if dupli_ob.object.type in ('LAMP', 'CAMERA', 'EMPTY', 'META', 'ARMATURE', 'LATTICE'):
+					continue
+				have_light = exportLights(l, scene, dupli_ob.object, dupli_ob.matrix)
 
-			areax = light.size
-
-			if light.shape == 'SQUARE':
-				areay = areax
-			elif light.shape == 'RECTANGLE':
-				areay = light.size_y
-			else:
-				areay = areax # not supported yet
-
-			points = [-areax/2, areay/2, 0.0, areax/2, areay/2, 0.0, areax/2, -areay/2, 0.0, -areax/2, -areay/2, 0.0]
-			shape_params = ParamSet() \
-				.add_integer('indices', [0, 1, 2, 0, 2, 3]) \
-				.add_point('P', points)
-			l.shape('trianglemesh', shape_params)
-			l.attributeEnd()
-
-			have_light = True
-			
-			continue
+			# free object dupli list again. Warning: all dupli objects are INVALID now!
+			if ob.dupli_list: 
+				ob.free_dupli_list()
+		else:
+			have_light = exportLights(l, scene, ob, ob.matrix)
 
 	return have_light
 		
