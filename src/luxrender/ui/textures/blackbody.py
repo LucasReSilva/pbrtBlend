20f504356e7aa48d7598163c30cc8cf977bb33a4
doug <doug@localhost>
2010-05-10 19:21:39 +0100

    add missing features return parameter from blackbody tex

--- src/luxrender/ui/textures/blackbody.py
+++ src/luxrender/ui/textures/blackbody.py
@@ -33,7 +33,7 @@ class blackbody(bpy.types.IDPropertyGroup):
 	
 	def get_paramset(self):
 		
-		return ParamSet().add_float('temperature', self.temperature)
+		return set(), ParamSet().add_float('temperature', self.temperature)
 
 class ui_panel_blackbody(luxrender_texture_base):
 	bl_label = 'LuxRender Blackbody Texture'
