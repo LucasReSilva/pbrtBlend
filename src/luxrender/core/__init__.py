e5b725f22b99883b7b473a38128211d060870fc8
dougal2 <dougal2@localhost>
2011-02-12 19:52:13 +0000

    add missing import

--- src/luxrender/core/__init__.py
+++ src/luxrender/core/__init__.py
@@ -67,8 +67,8 @@ from ..ui.materials import (
 from ..ui.textures import (
 	main, bilerp, blackbody, brick, cauchy, constant, checkerboard, dots,
 	equalenergy, fbm, gaussian, harlequin, imagemap, lampspectrum, luxpop,
-	marble, mix, sellmeier, sopra, uv, windy, wrinkled, mapping, tabulateddata,
-	transform
+	marble, mix, sellmeier, scale, sopra, uv, windy, wrinkled, mapping,
+	tabulateddata, transform
 )
 
 # Exporter Operators need to be imported to ensure initialisation
