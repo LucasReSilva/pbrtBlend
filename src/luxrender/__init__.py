d5c735441c5319230f093d3b6eea9af638601e43
Jens Verwiebe <JensVerwiebe@localhost>
2012-07-28 16:06:34 +0200

    Bump version to 1.0.0 RC3

--- src/luxrender/__init__.py
+++ src/luxrender/__init__.py
@@ -27,7 +27,7 @@
 bl_info = {
 	"name": "LuxRender",
 	"author": "LuxRender Project: Doug Hammond (dougal2), Asbjørn Heid (LordCrc), Daniel Genrich (Genscher), Jens Verwiebe, Jason Clarke (JtheNinja), neo2068",
-	"version": (1, 0, 0, 'RC2'),
+	"version": (1, 0, 0, 'RC3'),
 	"blender": (2, 6, 3),
 	"api": 44256,
 	"category": "Render",
