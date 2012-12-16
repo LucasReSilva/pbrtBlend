4fe8e5c25f2714e583a22b47d8bc940dc0582cee
Jason Clarke <JasonClarke@localhost>
2012-12-15 18:55:15 -0800

    Fix issue where lamp check would fail if the only lights were in linked dupligroups. Thanks to zukazuka for the patch Note: this will still fail if the layer the lit object was on in the original scene is not visible in the current scene, that still needs to be fixed

--- src/luxrender/export/scene.py
+++ src/luxrender/export/scene.py
@@ -27,6 +27,7 @@
 # System Libs
 import os
 import tempfile
+import bpy
 
 # Extensions_Framework Libs
 from extensions_framework import util as efutil
@@ -72,39 +73,53 @@ class SceneExporter(object):
 	def report(self, type, message):
 		LuxLog('%s: %s' % ('|'.join([('%s'%i).upper() for i in type]), message))
 	
-	def scene_is_lit(self, GE):
+	def object_is_lit(self,GE,obj):
+	
 		have_lamp = False
 		have_emitter = False
+	
+		if obj.type == 'LAMP' and GE.is_visible(obj):
+			lamp_enabled = export_lights.checkLightEnabled(self.scene, obj.data)
+			lamp_enabled &= obj.data.energy > 0.0
+			if obj.data.type == 'POINT':
+				lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_point.L_color.v > 0.0
+			if obj.data.type == 'SPOT':
+				lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_spot.L_color.v > 0.0
+			if obj.data.type == 'HEMI':
+				lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_hemi.L_color.v > 0.0
+			if obj.data.type == 'AREA':
+				lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_area.L_color.v > 0.0
+				lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_area.power > 0.0
+				lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_area.efficacy > 0.0
+			have_lamp |= lamp_enabled
+		 
+		if obj.type in ['MESH', 'SURFACE', 'CURVE', 'FONT'] and GE.is_visible(obj):
+			for ms in obj.material_slots:
+				mat = ms.material
+				if mat and mat.luxrender_emission.use_emission:
+					emit_enabled = self.scene.luxrender_lightgroups.is_enabled(mat.luxrender_emission.lightgroup)
+					emit_enabled &= (mat.luxrender_emission.L_color.v*mat.luxrender_emission.gain) > 0.0
+					have_emitter |= emit_enabled
+					if have_emitter:
+						break
+		 
+		if have_lamp or have_emitter:
+			return True
+
+		return False
+	
+	
+	def scene_is_lit(self, GE):
 		
 		for obj in self.scene.objects:
-			if obj.type == 'LAMP' and GE.is_visible(obj):
-				lamp_enabled = export_lights.checkLightEnabled(self.scene, obj.data)
-				lamp_enabled &= obj.data.energy > 0.0
-				if obj.data.type == 'POINT':
-					lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_point.L_color.v > 0.0
-				if obj.data.type == 'SPOT':
-					lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_spot.L_color.v > 0.0
-				if obj.data.type == 'HEMI':
-					lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_hemi.L_color.v > 0.0
-				if obj.data.type == 'AREA':
-					lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_area.L_color.v > 0.0
-					lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_area.power > 0.0
-					lamp_enabled &= obj.data.luxrender_lamp.luxrender_lamp_area.efficacy > 0.0
-				have_lamp |= lamp_enabled
-			
-			if obj.type in ['MESH', 'SURFACE', 'CURVE', 'FONT'] and GE.is_visible(obj):
-				for ms in obj.material_slots:
-					mat = ms.material
-					if mat and mat.luxrender_emission.use_emission:
-						emit_enabled = self.scene.luxrender_lightgroups.is_enabled(mat.luxrender_emission.lightgroup)
-						emit_enabled &= (mat.luxrender_emission.L_color.v*mat.luxrender_emission.gain*mat.luxrender_emission.power*mat.luxrender_emission.efficacy) >= 0.0
-						have_emitter |= emit_enabled
-						if have_emitter:
-							break
-			
-			if have_lamp or have_emitter:
+			if self.object_is_lit(GE,obj):
 				return True
-		
+	
+		for grp in bpy.data.groups:
+			for obj in list(grp.objects):
+				if self.object_is_lit(GE,obj):
+					return True
+			
 		return False
 	
 	def export(self):
