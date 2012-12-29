69d1090a7defc7addf8baaa0178aa84d2577fd2b
Jason Clarke <JasonClarke@localhost>
2012-12-29 12:06:03 -0800

    Fix part of mat UI not drawing in svn blender

--- src/luxrender/ui/materials/main.py
+++ src/luxrender/ui/materials/main.py
@@ -59,7 +59,10 @@ class ui_luxrender_material_header(luxrender_material_base):
 		if ob:
 			row = layout.row()
 
-			row.template_list(ob, "material_slots", ob, "active_material_index", rows=2)
+			if bpy.app.version < (2, 65, 3 ):
+				row.template_list(ob, "material_slots", ob, "active_material_index", rows=2)
+			else:
+				row.template_list("MATERIAL_UL_matslots", "", ob, "material_slots", ob, "active_material_index", rows=2)
 
 			col = row.column(align=True)
 			col.operator("object.material_slot_add", icon='ZOOMIN', text="")
