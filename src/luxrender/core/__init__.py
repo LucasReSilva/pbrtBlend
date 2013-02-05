509eabc5d180113d9304b1ce8fb0866d32c3bf8d
Jens Verwiebe <JensVerwiebe@localhost>
2013-02-05 15:16:48 +0100

    Remove the percentHaltThresholdComplete condition from sleephandling cause it is never reached, so pointless

--- src/luxrender/core/__init__.py
+++ src/luxrender/core/__init__.py
@@ -472,7 +472,7 @@ class RENDERENGINE_luxrender(bpy.types.RenderEngine):
 				# progressively update the preview
 				time.sleep(0.2) # safety-sleep
 				
-				if preview_context.getAttribute('renderer_statistics', 'samplesPerPixel') > 6 and not preview_context.getAttribute('renderer_statistics', 'percentHaltThresholdComplete') == 100:
+				if preview_context.getAttribute('renderer_statistics', 'samplesPerPixel') > 6:
 					if PREVIEW_TYPE == 'TEXTURE':
 						interruptible_sleep(0.8) # reduce update to every 1.0 sec until haltthreshold kills the render
 					else:
