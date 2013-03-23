e157eb4e020a89c7f7113925964a0c3b5b7ed348
Jason Clarke <JasonClarke@localhost>
2013-03-22 23:02:45 -0700

    Fixed use nodes button that got lost somewhere along the line

--- src/luxrender/ui/materials/main.py
+++ src/luxrender/ui/materials/main.py
@@ -82,6 +82,9 @@ class ui_luxrender_material_header(luxrender_material_base):
 			split.template_ID(ob, "active_material", new="material.new")
 			row = split.row()
 			
+			if mat:
+				row.prop(mat, "use_nodes", icon='NODETREE', text="")
+			
 			if slot:
 				row.prop(slot, "link", text="")
 			else:
