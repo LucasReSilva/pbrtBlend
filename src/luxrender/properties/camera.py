d000d20d1eab454927a678dffc93173198c55e0a
Jens Verwiebe <JensVerwiebe@localhost>
2013-08-10 18:04:08 +0200

    Set the outlierrejection to a less agressive default

--- src/luxrender/properties/camera.py
+++ src/luxrender/properties/camera.py
@@ -677,7 +677,7 @@ class luxrender_film(declarative_property_group):
 			'attr': 'outlierrejection_k',
 			'name': 'Firefly rejection',
 			'description': 'Firefly (outlier) rejection k parameter. 0=disabled',
-			'default': 5,
+			'default': 2,
 			'min': 0,
 			'soft_min': 0,
 		},
