68b81c716a78eb8a08e53a2ee67a48b7ad10b93d
Jens Verwiebe <JensVerwiebe@localhost>
2014-01-30 14:31:13 +0100

    Fix opencl.devices.select blank case ( core autouses all devices )

--- src/luxrender/properties/rendermode.py
+++ src/luxrender/properties/rendermode.py
@@ -258,7 +258,7 @@ class luxrender_rendermode(declarative_property_group):
 			
 			if self.opencl_prefs == True:
 				slg_gpu_workgroups = "opencl.gpu.workgroup.size = " + str(self.workgroupsize)
-				slg_devices_select =  "opencl.devices.select = " + self.deviceselection if self.deviceselection else "opencl.devices.select = " # blank
+				slg_devices_select =  "opencl.devices.select = " + self.deviceselection if self.deviceselection else "" # blank
 				slg_kernel_cache = "opencl.kernelcache = " + self.kernelcache
 				slg_params = '" "'.join((slg_params, slg_gpu_workgroups, slg_devices_select, slg_kernel_cache))
 			
