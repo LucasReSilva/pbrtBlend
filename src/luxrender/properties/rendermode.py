410a6e5683237bfb38d5d6f26a84a6f078f3ea2b
Jens Verwiebe <info@jensverwiebe.de>
2014-12-26 17:07:58 +0100

    Use the opencl device list panel also for luxcore api 1.0

--- src/luxrender/properties/rendermode.py
+++ src/luxrender/properties/rendermode.py
@@ -65,7 +65,7 @@ class luxrender_rendermode(declarative_property_group):
                           'rendermode': O(['hybridpath', 'hybridbidir', 'luxcorepathocl', 'luxcorebiaspathocl'])},
         'qbvhstacksize': {'opencl_prefs': True, 'renderer': 'hybrid'},
         'deviceselection': {'opencl_prefs': True,
-                            'rendermode': O(['hybridpath', 'hybridbidir', 'luxcorepathocl', 'luxcorebiaspathocl'])},
+                            'rendermode': O(['hybridpath', 'hybridbidir'])},
         'kernelcache': {'opencl_prefs': True, 'rendermode': O(['luxcorepathocl', 'luxcorebiaspathocl'])},
         'usegpus': {'rendermode': O(['hybridpath', 'hybridbidir', 'luxcorepathocl', 'luxcorebiaspathocl'])},
         'usecpus': {'rendermode': O(['luxcorepathocl', 'luxcorebiaspathocl'])},
@@ -280,8 +280,15 @@ class luxrender_rendermode(declarative_property_group):
 
             if self.opencl_prefs:
                 luxcore_gpu_workgroups = "opencl.gpu.workgroup.size = " + str(self.workgroupsize)
-                luxcore_devices_select = "opencl.devices.select = " + \
-                                         self.deviceselection if self.deviceselection else ""  # blank
+                import bpy
+                engine_settings = bpy.context.scene.luxcore_enginesettings
+                if len(engine_settings.luxcore_opencl_devices) > 0:
+                    dev_string = ''
+                    for dev_index in range(len(engine_settings.luxcore_opencl_devices)):
+                        dev = engine_settings.luxcore_opencl_devices[dev_index]
+                        dev_string += '1' if dev.opencl_device_enabled else '0'
+
+                luxcore_devices_select = "opencl.devices.select = " + dev_string
                 luxcore_params = '" "'.join(
                     (luxcore_params, luxcore_devices_select)) if luxcore_devices_select else luxcore_params
                 luxcore_kernel_cache = "opencl.kernelcache = " + self.kernelcache
