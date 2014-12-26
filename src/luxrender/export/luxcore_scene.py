5385ac659dbf6f1a699f56c9e8489053b4f27356
Jens Verwiebe <info@jensverwiebe.de>
2014-12-26 14:49:17 +0100

    Revert last commit, was nonsense

--- src/luxrender/export/luxcore_scene.py
+++ src/luxrender/export/luxcore_scene.py
@@ -1282,8 +1282,8 @@ class BlenderSceneConverter(object):
 
         gain_spectrum = [energy, energy, energy] # luxcore gain is spectrum !
 
-        if getattr(lux_lamp, 'L_color') and (
-                    light.type == 'SUN' and getattr(lux_lamp, 'sunsky_type') == 'distant'):
+        if getattr(lux_lamp, 'L_color') and not (
+                    light.type == 'SUN' and getattr(lux_lamp, 'sunsky_type') != 'distant'):
             iesfile = getattr(light.luxrender_lamp, 'iesname')
             iesfile, basename = get_expanded_file_name(light, iesfile)
             if iesfile != '':
