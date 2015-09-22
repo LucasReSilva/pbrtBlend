a2881c870cb5d85a0d223945a26257edde087f1c
Simon Wendsche <simonwendsche@gmx.de>
2015-09-22 22:43:36 +0200

    merge LuxCore Imagepipeline and LuxRender Film panels

--- src/luxrender/ui/camera.py
+++ src/luxrender/ui/camera.py
@@ -76,30 +76,10 @@ class film(camera_panel):
     display_property_groups = [
         ( ('camera', 'luxrender_camera'), 'luxrender_film', lambda: not UseLuxCore() ),
         ( ('camera', 'luxrender_camera', 'luxrender_film'), 'luxrender_colorspace', lambda: not UseLuxCore() ),
-        ( ('camera', 'luxrender_camera', 'luxrender_film'), 'luxrender_tonemapping',lambda: not UseLuxCore() ),
+        ( ('camera', 'luxrender_camera', 'luxrender_film'), 'luxrender_tonemapping', lambda: not UseLuxCore() ),
+        ( ('camera', 'luxrender_camera'), 'luxcore_imagepipeline_settings', lambda: UseLuxCore() ),
     ]
 
     def draw_crf_preset_menu(self, context):
         self.layout.menu('CAMERA_MT_luxrender_crf',
                          text=context.camera.luxrender_camera.luxrender_film.luxrender_colorspace.crf_preset)
-
-@LuxRenderAddon.addon_register_class
-class imagepipeline(camera_panel):
-    """
-    LuxCore Imagepipeline settings UI Panel
-    """
-
-    bl_label = 'LuxCore Imagepipeline'
-
-    display_property_groups = [
-        ( ('camera', 'luxrender_camera'), 'luxcore_imagepipeline_settings' ),
-    ]
-
-    def draw(self, context):
-        if UseLuxCore():
-            layout = self.layout
-            super().draw(context)
-
-    def draw_crf_preset_menu(self, context):
-        self.layout.menu('IMAGEPIPELINE_MT_luxrender_crf',
-                         text=context.camera.luxrender_camera.luxcore_imagepipeline_settings.crf_preset)
