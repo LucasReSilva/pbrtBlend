2eeeec6466ca37fe5c689d21ad9f34458ce29a36
Jens Verwiebe <info@jensverwiebe.de>
2016-10-02 13:48:35 +0200

    Register more panels needed since Blender 2.78

--- src/luxrender/core/__init__.py
+++ src/luxrender/core/__init__.py
@@ -142,10 +142,13 @@ if bpy.app.version > (2, 77, 2):
     _register_elm(bl_ui.properties_physics_fluid.PHYSICS_PT_fluid)
     _register_elm(bl_ui.properties_physics_rigidbody.PHYSICS_PT_rigid_body)
     _register_elm(bl_ui.properties_physics_rigidbody_constraint.PHYSICS_PT_rigid_body_constraint)
-    _register_elm(bl_ui.properties_physics_smoke.PHYSICS_PT_smoke)
     _register_elm(bl_ui.properties_physics_softbody.PHYSICS_PT_softbody)
     _register_elm(bl_ui.properties_physics_cloth.PHYSICS_PT_cloth)
-
+    _register_elm(bl_ui.properties_physics_smoke.PHYSICS_PT_smoke)
+    _register_elm(bl_ui.properties_physics_smoke.PHYSICS_PT_smoke_groups)
+    _register_elm(bl_ui.properties_physics_smoke.PHYSICS_PT_smoke_cache)
+    _register_elm(bl_ui.properties_physics_smoke.PHYSICS_PT_smoke_highres)
+    _register_elm(bl_ui.properties_physics_smoke.PHYSICS_PT_smoke_field_weights)
 
 # Some additions to Blender panels for better allocation in context
 # Use this example for such overrides
